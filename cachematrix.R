4eb676e9308b3f18cd380a9c980b464232ac7a59
Freeman5k <Freeman5k@gmail.com>
2016-02-24 18:50:44 -0500

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Below is the makeCacheMatrix 
 
 makeCacheMatrix <- function(x = matrix()) {
+  ocal_m <- NULL                                         
+  set <- function(y) {                                    
+    cache_x <<- y                                  
+    cache_m <<- NULL                                         
+  }
+  get <- function() cache_x                               
+  set_cache_m <- function(local_m) cache_m <<- local_m            
+  get_cache_m <- function() cache_m                       
+  list(set = set, get = get,
+       set_cache_m = set_cache_m,
+       get_cache_m = get_cache_m)
 
 }
 
 
-## Write a short comment describing this function
+## This is the cacheSolve matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+  local_m<- x$get_cache_m()               
+  if(!is.null(local_m)) {                 
+    message("getting cached data")  
+    return(local_m)
+  }                                       
+  startingmatrix <- x$get()                                        
+  endingmatrix <- solve(startingmatrix)   
+  x$set_cache_m(endingmatrix)             
+  endingmatrix 
 }
+
